d7b13c10b889477b60c48d40c61e4fa0c164fbad
Pett97 <henriquepeterson138@gmail.com>
2024-12-23 02:31:22 -0300

    atualizado script

--- criar_dominio_nginx.sh
+++ criar_dominio_nginx.sh
@@ -1,28 +1,63 @@
-
-#Peterson Henrique de Padua 
-#Pett97
-
 #!/bin/bash
 
-#  o nome do domínio
+#Peterson Henrique de Padua
+#Pett97
+
+# Solicitar o nome do domínio
 echo -e "\033[33mDigite o nome do domínio (exemplo: meu_dominio.local): \033[0m"
 read dominio
 
 # Definir o diretório onde os arquivos do site serão armazenados
 diretorio="/var/www/$dominio"
+html="$diretorio/html"
+status_pages="$diretorio/status-pages"
+logs="$diretorio/logs"
+maintenance="$diretorio/maintenance"
 
 # Verificar se o diretório já existe
 if [ -d "$diretorio" ]; then
   echo -e "\033[31mO diretório $diretorio já existe!\033[0m"
   exit 1
 else
-  # Criar o diretório para o domínio
-  mkdir -p "$diretorio"
-  echo -e "\033[32mDiretório $diretorio criado!\033[0m"
+  # Criar o diretório principal e a subpastas
+  mkdir -p "$html"
+  mkdir -p "$status_pages"
+  mkdir -p "$maintenance"
+  echo -e "\033[32mDiretório $html criado!\033[0m"
+  echo -e "\033[32mDiretório $status_page criado!\033[0m"
+  echo -e "\033[32mDiretório $maintenance criado!\033[0m"
 fi
 
-# Criar uma página de teste simples
-echo "<html><body><h1>Bem-vindo ao $dominio!</h1></body></html>" > "$diretorio/index.html"
+#verficar diretorio de logs
+if [ -d "$diretorio/logs" ]; then
+  echo -e "\033[31mO diretório $diretorio logs já existe!\033[0m"
+  exit 1
+else
+  # Criar o diretório logs
+  mkdir -p "$logs"
+  echo -e "\033[32mDiretório $logs criado!\033[0m"
+
+fi
+
+
+#criar arquivos de logs para registro
+touch "$logs/nginx_access.log"
+touch "$logs/nginx_error.log"
+echo -e "\033[32mArquivos de log criados!\033[0m"
+
+
+# Criar uma página de teste simples dentro da pasta html
+echo "<html><body><h1>Bem-vindo ao $dominio!</h1></body></html>" > "$html/index.html"
+
+#erros##################################
+
+#Criar Uma pagina de erro 404 
+echo "<html><body><h1>Erro 404 :/$dominio!</h1></body></html>" > "$status_pages/404.html"
+
+#Criar pagina manutencao
+echo "<html><body><h1>Manutencao 503 :/$dominio!</h1></body></html>" > "$maintenance/503.html"
+
+
 
 # Definir a configuração do Nginx para o domínio
 config_file="/etc/nginx/sites-available/$dominio"
@@ -32,14 +67,33 @@ server {
     listen 80;
     server_name $dominio;
 
-    root $diretorio;
+    root $html;
     index index.html;
 
-    access_log /var/log/nginx/$dominio.access.log;
-    error_log /var/log/nginx/$dominio.error.log;
+    access_log $logs/nginx_access.log;
+    error_log $logs/nginx_error.log;
 
     location / {
         try_files \$uri \$uri/ =404;
+    }
+
+    # Página de erro personalizada para 404
+    error_page 404 /status-pages/404.html;
+    location = /status-pages/404.html {
+        root $diretorio;
+        internal;
+    }
+
+    # Página de erro personalizada para 503
+    error_page 503 /maintenance/503.html;
+    location = /maintenance/503.html {
+        root $diretorio;
+        internal;
+    }
+
+    # Endpoint para ativar a página de manutenção
+    location /maintenance {
+        return 503;
     }
 }
 EOL
@@ -50,20 +104,19 @@ ln -s $config_file /etc/nginx/sites-enabled/
 # Adicionar o domínio ao arquivo /etc/hosts
 if ! grep -q "$dominio" /etc/hosts; then
     echo "127.0.0.1   $dominio" >> /etc/hosts
-    echo -e "\033[32mDomínio $dominio adicionado ao /etc/hosts\033[0m"  # Mensagem verde
+    echo -e "\033[32mDomínio $dominio adicionado ao /etc/hosts\033[0m"
 else
-    echo -e "\033[31mO domínio $dominio já está presente no /etc/hosts\033[0m"  # Mensagem vermelha
+    echo -e "\033[31mO domínio $dominio já está presente no /etc/hosts\033[0m"
 fi
 
-#  testa configuracao
+# Testar a configuração do Nginx
 nginx -t
 
-# testa se esta certo a configuracao
+# Verificar se a configuração está correta e recarregar o Nginx
 if [ $? -eq 0 ]; then
   systemctl reload nginx
-  echo -e "\033[32mDomínio $dominio configurado com sucesso!\033[0m"  # Mensagem verde
+  echo -e "\033[32mDomínio $dominio configurado com sucesso!\033[0m"
 else
-  echo -e "\033[31mErro na configuração do Nginx. Não foi possível recarregar o serviço.\033[0m"  # Mensagem vermelha
+  echo -e "\033[31mErro na configuração do Nginx. Não foi possível recarregar o serviço.\033[0m"
   exit 1
 fi
-
--- readme.md
+++ readme.md
@@ -1,3 +1,25 @@
+**instalar o nginx**
+sudo apt-get install nginx
+
+**parar o apache2**
+sudo systemctl stop apache2
+
+**como Executar**
+no diretorio onde foi clonado
+
+**habilitar a execucao**
+sudo chmod +x criar_dominio_nginx.sh 
+
+**executar**
+sudo ./criar_dominio_nginx.sh 
+
 **Esse Script ira criar dominios locais do nginx**
 
-****nao cria pagina de logs e etc**
+**Paginas criadas**
+index.html
+erro404
+erro503
+
+
+**Esse script não atleta o arquivo nginx.conf**
+caso nessario tem alterar o mesmo manualmente
